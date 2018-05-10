122d54bca6ac8c02e04ea5409786c8f44f56347a
Amrit Sarkar <sarkaramrit2@gmail.com>
2018-05-10 16:44:08 +0530

    Update BulkIndexer3.java

--- src/main/java/org/apache/solr/BulkIndexer3.java
+++ src/main/java/org/apache/solr/BulkIndexer3.java
@@ -27,7 +27,6 @@ public class BulkIndexer3 {
     public static void main(String args[]) throws Exception {
 
         final String zkHost = "localhost:9983";
-        //final String zkHost = "apple:9983";
         final CloudSolrClient client = new CloudSolrClient(zkHost);
         final String collection = "test";
         client.setDefaultCollection(collection);
